2e78b63db5a26bf5821762bfbedea420e6a395b4
Balram Pandey <balrampandey19@gmail.com>
2017-05-21 02:23:57 +0530

    layout fix

--- app/src/main/java/balram/com/hackernews/MainActivity.kt
+++ app/src/main/java/balram/com/hackernews/MainActivity.kt
@@ -2,11 +2,16 @@ package balram.com.hackernews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById(R.id.main_listview) as ListView
+
     }
 }
