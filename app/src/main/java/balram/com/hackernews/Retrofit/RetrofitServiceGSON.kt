369be4d73f70939ecce46cc1f697c922e24db6a9
Balram Pandey <balrampandey19@gmail.com>
2017-05-23 00:09:42 +0530

    fix

--- app/src/main/java/balram/com/hackernews/Retrofit/RetrofitServiceGSON.kt
+++ app/src/main/java/balram/com/hackernews/Retrofit/RetrofitServiceGSON.kt
@@ -3,7 +3,6 @@ package balram.com.hackernews.Retrofit
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 /**
