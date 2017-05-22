f37179aab48b4687f4122210add04fd6397ce119
Balram Pandey <balrampandey19@gmail.com>
2017-05-23 00:02:12 +0530

    Retrofit services kotlin

---
+++ app/src/main/java/balram/com/hackernews/Retrofit/RetrofitServiceGSON.kt
@@ -0,0 +1,36 @@
+package balram.com.hackernews.Retrofit
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by Balram Pandey
+ */
+object RetrofitServiceGSON {
+
+    val API_BASE_URL = "https://www.googleapis.com/youtube/v3/"
+    private val logging = HttpLoggingInterceptor()
+
+    private val httpClient = OkHttpClient.Builder();
+
+    private val builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+
+    fun <S> createService(serviceClass: Class<S>): S {
+
+        httpClient.addInterceptor { chain ->
+            var request = chain.request()
+                request = request.newBuilder().header("Cache-Control", "public, max-age=" + 60).build()
+            chain.proceed(request)
+        }
+
+        val client = httpClient.build()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        httpClient.addInterceptor(logging)
+        val retrofit = builder.client(client).build()
+        return retrofit.create(serviceClass)
+    }
+}
