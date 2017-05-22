bf58187849feb2fe9f2d5bcdb180f0fd350b34b1
Balram Pandey <balrampandey19@gmail.com>
2017-05-22 23:55:10 +0530

    add internet permission

--- app/src/main/java/balram/com/hackernews/MainActivity.kt
+++ app/src/main/java/balram/com/hackernews/MainActivity.kt
@@ -2,7 +2,6 @@ package balram.com.hackernews
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var listView = findViewById(R.id.main_listview) as ListView
+        var listView= findViewById(R.id.news_list) as ListView
 
     }
 }
--- app/src/main/java/balram/com/hackernews/Retrofit/NewsAPI.kt
+++
@@ -1,6 +0,0 @@
-package balram.com.hackernews.Retrofit
-
-
-interface NewsAPI {
-    fun getNews(after: String, limit: String): retrofit2.Call<RedditNewsResponse>
-}
